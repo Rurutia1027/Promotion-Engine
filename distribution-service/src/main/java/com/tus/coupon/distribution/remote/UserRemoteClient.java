86eaa87df0614839363c3745b9eceae24cfd7ced
Rurutia1027 <rurutia1027@gmail.com>
2026-04-23 20:59:19 +0800

    add todos for future replace feign url addresses

--- distribution-service/src/main/java/com/tus/coupon/distribution/remote/UserRemoteClient.java
+++ distribution-service/src/main/java/com/tus/coupon/distribution/remote/UserRemoteClient.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(
         name = "coupon-user",
+        // TODO: Switch to Consul service discovery only; remove explicit URL override once
+        //  Consul registration is enabled
         url = "${coupon.distribution.feign.remote-url.user:http://127.0.0.1:10031}"
 )
 public interface UserRemoteClient extends UserApi {
